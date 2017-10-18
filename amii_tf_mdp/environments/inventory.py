dad2539c4a2684b4bee3c79dfd9e2bfca6207262
Dustin Morrill <dmorrill10@gmail.com>
2017-10-18 15:05:25 -0600

    Simplified InventoryMdpGenerator a little.

--- amii_tf_mdp/environments/inventory.py
+++ amii_tf_mdp/environments/inventory.py
@@ -1,6 +1,5 @@
-from scipy.stats import norm
-import numpy as np
 import tensorflow as tf
+from amii_tf_nn.projection import l1_projection_to_simplex
 
 
 class InventoryMdpGenerator(object):
@@ -45,29 +44,7 @@ class InventoryMdpGenerator(object):
                         )
 
     @classmethod
-    def gaussian_demand_np(cls, mean, std):
-        def prob_of_demand_when_inventory_clears(d):
-            lb = d - 0.5
-            return 1.0 - norm.cdf(lb, loc=mean, scale=std)
-
-        def prob_of_demand_when_inventory_remains(d):
-            lb = d - 0.5
-            ub = d + 0.5
-            return (
-                norm.cdf(
-                    ub,
-                    loc=mean,
-                    scale=std
-                ) -
-                norm.cdf(lb, loc=mean, scale=std)
-            )
-        return (
-            prob_of_demand_when_inventory_clears,
-            prob_of_demand_when_inventory_remains
-        )
-
-    @classmethod
-    def gaussian_demand_tf(cls, mean, std):
+    def gaussian_demand(cls, mean, std):
         def prob_of_demand_when_inventory_clears(d):
             lb = d - 0.5
             return 1.0 - tf.contrib.distributions.Normal(mean, std).cdf(lb)
@@ -99,58 +76,35 @@ class InventoryMdpGenerator(object):
 
     def num_actions(self): return self.num_states()
 
-    def fraction_of_max_inventory_gaussian_demand_np(self, fraction):
+    def fraction_of_max_inventory_gaussian_demand(self, fraction):
         mean = fraction * self.max_inventory
-        return self.__class__.gaussian_demand_np(
-            mean,
-            min(mean, self.max_inventory - mean) / 3.0
-        )
-
-    def fraction_of_max_inventory_gaussian_demand_tf(self, fraction):
-        mean = fraction * self.max_inventory
-        return self.__class__.gaussian_demand_tf(
+        return self.__class__.gaussian_demand(
             mean,
             tf.minimum(mean, self.max_inventory - mean) / 3.0
         )
 
-    def transition_and_rewards_np(
-        self,
-        prob_of_demand_when_inventory_clears,
-        prob_of_demand_when_inventory_remains
-    ):
-        '''
-        TODO This function is really slow right now since it uses nested
-        loops and isn't in TensorFlow.
-        '''
-        resale_cost = self.resale_cost()
-        maintenance_cost = self.maintenance_cost()
-        num_states = self.num_states()
-        num_actions = self.num_actions()
+    def root(self):
+        return l1_projection_to_simplex(
+            tf.random_uniform((self.num_states(),))
+        )
 
-        R = np.zeros([num_states, num_actions, num_states])
-        T = np.zeros([num_states, num_actions, num_states])
-        for s in range(num_states):
-            for a in range(num_actions):
-                usable_inventory = min(s + a, self.max_inventory)
-                restocking_cost = self.wholesale_cost * a
+    def rewards(self):
+        if self.cache is None:
+            self.cache = self.__class__.Cache(
+                self.resale_cost(),
+                self.maintenance_cost(),
+                self.num_states(),
+                self.num_actions(),
+                self.max_inventory,
+                self.wholesale_cost
+            )
+        return tf.scatter_nd(
+            self.cache.indices,
+            self.cache.R_vals,
+            shape=(self.num_states(), self.num_actions(), self.num_states())
+        )
 
-                T[s, a, 0] = prob_of_demand_when_inventory_clears(
-                    usable_inventory
-                )
-                R[s, a, 0] = resale_cost * usable_inventory - restocking_cost
-                for s_prime in range(1, usable_inventory + 1):
-                    d = usable_inventory - s_prime
-                    T[s, a, s_prime] = prob_of_demand_when_inventory_remains(d)
-                    R[s, a, s_prime] = (
-                        resale_cost * d -
-                        restocking_cost -
-                        maintenance_cost * s_prime
-                    )
-        T = T / T.sum(axis=2)
-        T[np.isnan(T)] = 0.0
-        return (T, R)
-
-    def transition_and_rewards_tf(
+    def transitions(
         self,
         prob_of_demand_when_inventory_clears,
         prob_of_demand_when_inventory_remains
@@ -177,11 +131,6 @@ class InventoryMdpGenerator(object):
             ),
             shape=(self.num_states(), self.num_actions(), self.num_states())
         )
-        R = tf.scatter_nd(
-            self.cache.indices,
-            self.cache.R_vals,
-            shape=(self.num_states(), self.num_actions(), self.num_states())
-        )
 
         z = tf.reduce_sum(T, axis=2)
         z = tf.where(
@@ -189,5 +138,4 @@ class InventoryMdpGenerator(object):
             z,
             tf.ones_like(z)
         )
-        T = T / z
-        return (T, R)
+        return T / z
