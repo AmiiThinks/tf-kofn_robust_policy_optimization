- Add state successor op. - Fix alpha computation for gridworld drawing. - Allow thresholds to be set for the display of policy arrows and distribution overlays for gridworld diagrams. - Improve gridworld experiment

amii_tf_mdp/discounted_mdp.py
@@ -13,8 +13,10 @@ def state_action_successor_policy_evaluation_op(P,
     P = tf.convert_to_tensor(P)
     num_states = P.shape[1].value
     num_actions = int(P.shape[0].value / num_states)
-    ones = tf.ones([num_states * num_actions, num_states * num_actions])
-    H_0 = (ones * 1.0 / (num_states * num_actions))
+    H_0 = tf.constant(
+        1.0 / (num_states * num_actions),
+        shape=(num_states * num_actions, num_states * num_actions)
+    )
 
     def H_dp1_op(H_d):
         return (((1 - gamma) * tf.eye(H_d.shape[0].value)) +
@@ -192,3 +194,36 @@ def associated_ops(action_weights,
         Pi, root_op, transition_model_op, reward_model_op, **kwargs)
 
     return Pi, action_values_op, state_values_op, ev_op
+
+
+def state_successor_policy_evaluation_op(P,
+                                         Pi,
+                                         gamma=0.9,
+                                         threshold=1e-15,
+                                         max_num_iterations=-1):
+    P = tf.convert_to_tensor(P)
+    num_states = P.shape[1].value
+    M_0 = tf.constant(
+        1.0 / num_states,
+        shape=(num_states, num_states)
+    )
+
+    def M_dp1_op(M_d):
+        return (((1 - gamma) * tf.eye(M_d.shape[0].value)) +
+                (gamma * Pi @ P @ M_d))
+
+    def error_above_threshold(M_d, M_dp1):
+        return tf.reduce_sum(tf.abs(M_dp1 - M_d)) > threshold
+
+    def cond(d, M_d, M_dp1):
+        error_op = error_above_threshold(M_d, M_dp1)
+        return tf.squeeze(
+            tf.logical_or(
+                tf.logical_and(max_num_iterations < 1, error_op),
+                tf.logical_and(tf.less(d, max_num_iterations), error_op)))
+
+    return tf.while_loop(
+        cond,
+        lambda d, _, M_d: [d + 1, M_d, M_dp1_op(M_d)],
+        [1, M_0, M_dp1_op(M_0)],
+        parallel_iterations=1)[-1]

amii_tf_mdp/environments/gridworld.py
@@ -17,12 +17,11 @@ class Gridworld(object):
     def num_cardinal_directions(): return 4
 
     class Painter(object):
-        MIN_ALPHA = 0.2
+        MIN_ALPHA = 0.1
 
         @classmethod
         def weight_to_alpha(cls, weight):
-            return (weight + cls.MIN_ALPHA) / (1 - cls.MIN_ALPHA)
-
+            return (weight + cls.MIN_ALPHA) / (1 + cls.MIN_ALPHA)
 
         class Tiles(object):
             '''
@@ -75,7 +74,8 @@ class Gridworld(object):
                 cls,
                 row_column_policy,
                 color='black',
-                sink=None
+                sink=None,
+                threshold=1e-15
             ):
                 patches = []
                 for (row, column, direction), weight in np.ndenumerate(
@@ -87,7 +87,7 @@ class Gridworld(object):
                             sink[0] != row or
                             sink[1] != column
                         ) and
-                        weight > 0.0
+                        weight > threshold
                     ):
                         pos = np.array((column, row)) + 1
                         patches.append(
@@ -133,13 +133,14 @@ class Gridworld(object):
         @classmethod
         def distribution_overlay_patches(
             cls,
-            discounted_row_column_distribution
+            discounted_row_column_distribution,
+            threshold=1e-15
         ):
             patches = []
             for (row, column), weight in np.ndenumerate(
                 discounted_row_column_distribution
             ):
-                if weight > 0:
+                if weight > threshold:
                     pos = np.array((column, row)) + 1
                     patches.append(
                         mpl.patches.Rectangle(
@@ -157,7 +158,8 @@ class Gridworld(object):
         def draw(
             row_column_policy,
             discounted_row_column_distribution=None,
-            plt=plt
+            plt=plt,
+            threshold=1e-15
         ):
             grid_img = Gridworld.Painter.Tiles.grid_rgb_img(
                 row_column_policy.shape[0],
@@ -183,7 +185,8 @@ class Gridworld(object):
             plt.imshow(grid_img)
 
             for patch in Gridworld.Painter.distribution_overlay_patches(
-                discounted_row_column_distribution
+                discounted_row_column_distribution,
+                threshold=threshold
             ):
                 ax.add_patch(patch)
 
@@ -355,7 +358,8 @@ if __name__ == '__main__':
     num_rows = 3
     num_columns = 10
 
-    k_200_policy = np.array([[ 0.        ,  1.        ,  0.        ,  0.        ],
+    k_200_policy = np.array(
+      [[ 0.        ,  1.        ,  0.        ,  0.        ],
        [ 0.        ,  1.        ,  0.        ,  0.        ],
        [ 0.        ,  1.        ,  0.        ,  0.        ],
        [ 0.        ,  1.        ,  0.        ,  0.        ],
@@ -377,23 +381,31 @@ if __name__ == '__main__':
        [ 1.        ,  0.        ,  0.        ,  0.        ],
        [ 1.        ,  0.        ,  0.        ,  0.        ],
        [ 1.        ,  0.        ,  0.        ,  0.        ],
-       [ 0.70766139,  0.        ,  0.        ,  0.29233864],
-       [ 0.41930115,  0.        ,  0.        ,  0.58069885],
-       [ 0.30641049,  0.        ,  0.        ,  0.69358951],
-       [ 0.25235799,  0.        ,  0.        ,  0.74764198],
-       [ 0.26399311,  0.        ,  0.        ,  0.73600686],
-       [ 0.21423712,  0.        ,  0.        ,  0.78576285],
-       [ 0.15432464,  0.        ,  0.        ,  0.84567541],
-       [ 0.15158875,  0.        ,  0.        ,  0.8484112 ]],)
+       [ 0.74676859,  0.        ,  0.        ,  0.25323141],
+       [ 0.53854144,  0.        ,  0.        ,  0.46145853],
+       [ 0.3968643 ,  0.        ,  0.        ,  0.6031357 ],
+       [ 0.35593784,  0.        ,  0.        ,  0.64406216],
+       [ 0.32623151,  0.        ,  0.        ,  0.67376852],
+       [ 0.23172346,  0.        ,  0.        ,  0.76827651],
+       [ 0.24368413,  0.        ,  0.        ,  0.75631583],
+       [ 0.15257818,  0.        ,  0.        ,  0.84742177]])
     row_column_policy = k_200_policy.reshape([num_rows, num_columns, 4])
 
-    weights = np.maximum(
-        0.0,
-        np.random.normal(size=row_column_policy.shape[:2])
-    )
     fig, ax = Gridworld.Painter.draw(
         row_column_policy,
-        weights / weights.sum()
+        np.array([[  1.40720793e-17,   5.25108597e-04,   2.19317898e-03,
+          4.96153440e-03,   8.52138456e-03,   1.39449192e-02,
+          2.20358949e-02,   2.95764264e-02,   4.16728668e-02,
+          4.98644024e-01],
+       [  1.40720793e-17,   5.83454152e-04,   1.91175693e-03,
+          3.31963645e-03,   4.50667180e-03,   6.97296951e-03,
+          1.05394088e-02,   1.08268056e-02,   1.67267621e-02,
+          1.37320356e-02],
+       [  1.40720793e-17,   6.48282294e-04,   2.84448825e-03,
+          6.84902770e-03,   1.26174437e-02,   2.17671245e-02,
+          3.58961523e-02,   5.19143939e-02,   7.62679577e-02,
+          1.00000001e-01]]),
+        threshold=1e-10
     )
 
     plt.show()

amii_tf_mdp/robust/uncertain_reward_discounted_continuing_kofn.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-import numpy as np  # For eigen decomposition to get steady state distribution
 from amii_tf_mdp.discounted_mdp import inst_regrets_op, associated_ops, \
-    value_ops
+    value_ops, state_successor_policy_evaluation_op
 from amii_tf_mdp.utils.tensor import row_normalize_op
 
 
@@ -71,12 +70,22 @@ class UncertainRewardDiscountedContinuingKofn(object):
         self.policy_op = row_normalize_op(self.advantages_op)
 
         '''
-        Probability of transitioning to each state from each state under
-        this policy and transition model.
+        Discounted state successor distribution.
 
         |States| by |States| Tensor.
         '''
-        self.state_to_state_transitions_op = self.Pi_op @ transition_model_op
+        self.state_successor_op = state_successor_policy_evaluation_op(
+            transition_model_op, self.Pi_op, gamma=gamma)
+
+        '''
+        Probability of terminating in each state.
+
+        |States| by 1 Tensor
+        '''
+        self.state_distribution_op = tf.matmul(
+            tf.transpose(self.state_successor_op), root_op
+        )
+
 
     def num_states(self): return self.root_op.shape[0].value
 

exe/safe_rl_gridworld_k_of_n.py
@@ -5,7 +5,8 @@ import math
 import numpy as np
 from amii_tf_mdp.robust.k_of_n import DeterministicKofnConfig
 from amii_tf_mdp.discounted_mdp import generalized_policy_iteration_op, \
-    associated_ops, value_ops
+    associated_ops, value_ops, \
+    state_successor_policy_evaluation_op
 from amii_tf_mdp.utils.timer import Timer
 from amii_tf_mdp.utils.quadrature import midpoint_quadrature
 from amii_tf_mdp.utils.experiment import PickleExperiment
@@ -203,8 +204,8 @@ def train_and_save_k_of_n(*methods):
                     'midpoint_area': area
                 },
                 'policy': sess.run(methods[i].policy_op),
-                'state_to_state_transitions': (
-                    sess.run(methods[i].state_to_state_transitions_op)
+                'state_distribution': sess.run(
+                    methods[i].state_distribution_op
                 )
             }
             if len(evs) > 0:
@@ -260,7 +261,16 @@ def eval_baseline(root_op,
         'midpoint_area': area
     },
     d['policy'] = sess.run(policy_op),
-    d['state_to_state_transitions'] = sess.run(Pi @ transition_model_op)
+    d['state_distribution'] = sess.run(
+        tf.matmul(
+            tf.transpose(
+                state_successor_policy_evaluation_op(
+                    transition_model_op,
+                    Pi,
+                    gamma=gamma)),
+            root_op
+        )
+    )
 
 
 total_timer = Timer('Experiment')

test/discounted_mdp_test.py
@@ -4,7 +4,8 @@ from amii_tf_mdp.discounted_mdp import root_value_op, \
     primal_action_value_policy_evaluation_op, \
     dual_action_value_policy_evaluation_op, \
     regret_matching_op, \
-    generalized_policy_iteration_op
+    generalized_policy_iteration_op, \
+    state_successor_policy_evaluation_op
 from amii_tf_mdp.utils.tensor import row_normalize_op, \
     matrix_to_block_matrix_op
 from amii_tf_mdp.utils.random import reset_random_state
@@ -183,6 +184,72 @@ class DiscountedMdpTest(tf.test.TestCase):
                                            row_normalize_op(
                                                tf.ones([num_states, 1])),
                                            Pi_5_op @ q_op)).eval())
+
+    def test_recover_state_distribution_from_state_action_distribution(self):
+        num_states = 3
+        num_actions = 2
+        gamma = 0.9
+        with self.test_session() as sess:
+            reset_random_state(101)
+
+            policy_op = row_normalize_op(
+                [
+                    [1.0, 2.0],
+                    [3.0, 0.0],
+                    [5.0, 6.0]
+                ]
+            )
+            indices_op = tf.stack(
+                (
+                    tf.range(num_states, dtype=tf.int64),
+                    tf.argmax(policy_op, axis=1)
+                ),
+                axis=1
+            )
+            non_zero_probs_op = tf.gather_nd(policy_op, indices_op)
+
+            self.assertAllClose(
+                [2 / 3.0, 1.0, 0.54545456],
+                non_zero_probs_op.eval()
+            )
+
+            Pi_op = matrix_to_block_matrix_op(policy_op)
+            P = sess.run(
+                row_normalize_op(
+                    tf.random_normal(
+                        shape=[num_states * num_actions, num_states])))
+
+            # This only works when H is very close to the true state-action
+            # distribution.
+            H_op = state_action_successor_policy_evaluation_op(
+                P, Pi_op, gamma)
+
+            A_op = tf.matmul(Pi_op, H_op)
+
+            columns_to_gather_op = (
+                indices_op[:, 1] + num_actions * indices_op[:, 0]
+            )
+            thin_A_op = tf.transpose(
+                tf.gather(
+                    tf.transpose(A_op),
+                    columns_to_gather_op
+                )
+            )
+
+            M_op = thin_A_op / non_zero_probs_op
+            sum_M_op = tf.reduce_sum(M_op, axis=1)
+            self.assertAllClose(
+                tf.ones_like(sum_M_op).eval(),
+                sum_M_op.eval()
+            )
+            self.assertAllClose(
+                A_op.eval(),
+                tf.matmul(M_op, Pi_op).eval()
+            )
+            self.assertAllClose(
+                M_op.eval(),
+                state_successor_policy_evaluation_op(P, Pi_op, gamma).eval()
+            )
 
 
 if __name__ == '__main__':