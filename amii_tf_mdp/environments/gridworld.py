3d52a7b4dc0f7b3086dd198098be736be418dd84
Dustin Morrill <dmorrill10@gmail.com>
2017-12-09 08:39:50 -0700

    Improve gridworld drawing functions.

--- amii_tf_mdp/environments/gridworld.py
+++ amii_tf_mdp/environments/gridworld.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 
 class Gridworld(object):
@@ -16,6 +17,13 @@ class Gridworld(object):
     def num_cardinal_directions(): return 4
 
     class Painter(object):
+        MIN_ALPHA = 0.2
+
+        @classmethod
+        def weight_to_alpha(cls, weight):
+            return (weight + cls.MIN_ALPHA) / (1 - cls.MIN_ALPHA)
+
+
         class Tiles(object):
             '''
             Based on the color scheme used by
@@ -29,21 +37,22 @@ class Gridworld(object):
             PATH_WHITE = [1.0, 1.0, 1.0]
             SOURCE_ORANGE = [1.0, 130 / 255.0, 0.0]
 
-            @staticmethod
+            @classmethod
             def grid_rgb_img(
+                cls,
                 num_rows,
                 num_columns,
                 source=(-2, -2),
-                goal=(1, 2)
+                goal=(1, -2)
             ):
                 grid = np.ones([num_rows + 2, num_columns + 2, 3])
-                grid[:, 0] = WALL_GREY
-                grid[:, -1] = WALL_GREY
-                grid[0, :] = WALL_GREY
-                grid[-1, :] = WALL_GREY
-                grid[source] = SOURCE_ORANGE
-                grid[goal] = GOAL_GREEN
-                grid[2, 2:-1] = UNCERTAINTY_YELLOW
+                grid[:, 0] = cls.WALL_GREY
+                grid[:, -1] = cls.WALL_GREY
+                grid[0, :] = cls.WALL_GREY
+                grid[-1, :] = cls.WALL_GREY
+                grid[source] = cls.SOURCE_ORANGE
+                grid[goal] = cls.GOAL_GREEN
+                grid[2, 2:-1] = cls.UNCERTAINTY_YELLOW
                 return grid
 
         class PolicyWeights(object):
@@ -61,19 +70,33 @@ class Gridworld(object):
                 NORTH_OFFSET, EAST_OFFSET, SOUTH_OFFSET, WEST_OFFSET
             ]
 
-            def policy_patches(row_column_policy, color='black', sink=None):
+            @classmethod
+            def policy_patches(
+                cls,
+                row_column_policy,
+                color='black',
+                sink=None
+            ):
                 patches = []
                 for (row, column, direction), weight in np.ndenumerate(
                     row_column_policy
                 ):
-                    if sink is None or sink[0] != row or sink[1] != column:
+                    if (
+                        (
+                            sink is None or
+                            sink[0] != row or
+                            sink[1] != column
+                        ) and
+                        weight > 0.0
+                    ):
                         pos = np.array((column, row)) + 1
                         patches.append(
                             mpl.patches.Polygon(
                                 pos +
-                                DIRECTION_OFFSETS[direction],
+                                cls.DIRECTION_OFFSETS[direction],
                                 closed=True,
-                                alpha=weight,
+                                alpha=Gridworld.Painter.weight_to_alpha(
+                                    weight),
                                 facecolor=color
                             )
                         )
@@ -84,7 +107,7 @@ class Gridworld(object):
             row,
             column,
             label,
-            plt=mpl.pyplot,
+            plt=plt,
             fontsize=7
         ):
             return plt.text(
@@ -97,7 +120,7 @@ class Gridworld(object):
             )
 
         @staticmethod
-        def add_center_label(row, column, label, plt=mpl.pyplot, fontsize=5):
+        def add_center_label(row, column, label, plt=plt, fontsize=5):
             return plt.text(
                 column,
                 row,
@@ -107,14 +130,41 @@ class Gridworld(object):
                 fontsize=fontsize
             )
 
+        @classmethod
+        def distribution_overlay_patches(
+            cls,
+            discounted_row_column_distribution
+        ):
+            patches = []
+            for (row, column), weight in np.ndenumerate(
+                discounted_row_column_distribution
+            ):
+                if weight > 0:
+                    pos = np.array((column, row)) + 1
+                    patches.append(
+                        mpl.patches.Rectangle(
+                            pos - np.array((1.0 / 3, 1.0 / 3)),
+                            width=2.0 / 3,
+                            height=2.0 / 3,
+                            fill=True,
+                            facecolor=cls.Tiles.AGENT_BLUE,
+                            alpha=cls.weight_to_alpha(weight)
+                        )
+                    )
+            return patches
+
         @staticmethod
-        def draw(row_column_policy, plt=mpl.pyplot):
-            grid_img = Tiles.grid_rgb_img(
+        def draw(
+            row_column_policy,
+            discounted_row_column_distribution=None,
+            plt=plt
+        ):
+            grid_img = Gridworld.Painter.Tiles.grid_rgb_img(
                 row_column_policy.shape[0],
                 row_column_policy.shape[1]
             )
 
-            plt.rc('grid', linestyle='-', color=path_grey)
+            plt.rc('grid', linestyle='-', color=Gridworld.Painter.Tiles.GRID_GREY)
             fig = plt.figure()
             ax = fig.add_subplot(111)
             plt.grid(True)
@@ -123,35 +173,39 @@ class Gridworld(object):
             ax.set_xticklabels([])
             ax.set_yticklabels([])
             for tic in ax.xaxis.get_major_ticks():
-            tic.tick1On = tic.tick2On = False
+                tic.tick1On = tic.tick2On = False
             for tic in ax.yaxis.get_major_ticks():
-            tic.tick1On = tic.tick2On = False
+                tic.tick1On = tic.tick2On = False
 
             ax.set_xticks(np.arange(0.5, num_columns + 1.5))
             ax.set_yticks(np.arange(0.5, num_rows + 1.5))
 
-            plt.imshow(grid)
+            plt.imshow(grid_img)
 
-            for patch in PolicyWeights.policy_patches(
+            for patch in Gridworld.Painter.distribution_overlay_patches(
+                discounted_row_column_distribution
+            ):
+                ax.add_patch(patch)
+
+            for patch in Gridworld.Painter.PolicyWeights.policy_patches(
                 row_column_policy,
                 sink=(0, row_column_policy.shape[1] - 1)
             ):
                 ax.add_patch(patch)
 
-            Painter.add_top_left_corner_label(
+            Gridworld.Painter.add_top_left_corner_label(
                 row_column_policy.shape[0],
                 row_column_policy.shape[1],
                 r'$S$'
             )
-            Painter.add_top_left_corner_label(
-                1,
-                row_column_policy.shape[1],
-                r'$G$'
-            )
-            Painter.add_center_label(1, row_column_policy.shape[1], '0.1')
+            Gridworld.Painter.add_top_left_corner_label(
+                1, row_column_policy.shape[1], r'$G$')
+            Gridworld.Painter.add_center_label(
+                1, row_column_policy.shape[1], '0.1')
 
             for c in range(1, row_column_policy.shape[1]):
-                Painter.add_center_label(2, c + 1, r"$\sigma={}$".format(0.1))
+                Gridworld.Painter.add_center_label(
+                    2, c + 1, r"$\sigma={}$".format(0.1))
             return fig, ax
 
     def __init__(self, num_rows, num_columns):
@@ -213,9 +267,8 @@ class Gridworld(object):
             ),
             (self.num_rows * self.num_columns, 1)
         )
-        state_action_rewards_op = (
-            state_actions_to_state_model_op @ state_rewards_op
-        )
+        state_action_rewards_op = tf.matmul(
+            state_actions_to_state_model_op, state_rewards_op)
         if sink is not None:
             temp = tf.reshape(
                 state_action_rewards_op,
@@ -296,3 +349,51 @@ class Gridworld(object):
             tf.ones([indices.shape[0]]),
             shape=intuitive_shape
         )
+
+
+if __name__ == '__main__':
+    num_rows = 3
+    num_columns = 10
+
+    k_200_policy = np.array([[ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.        ,  1.        ,  0.        ,  0.        ],
+       [ 0.25      ,  0.25      ,  0.25      ,  0.25      ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 1.        ,  0.        ,  0.        ,  0.        ],
+       [ 0.70766139,  0.        ,  0.        ,  0.29233864],
+       [ 0.41930115,  0.        ,  0.        ,  0.58069885],
+       [ 0.30641049,  0.        ,  0.        ,  0.69358951],
+       [ 0.25235799,  0.        ,  0.        ,  0.74764198],
+       [ 0.26399311,  0.        ,  0.        ,  0.73600686],
+       [ 0.21423712,  0.        ,  0.        ,  0.78576285],
+       [ 0.15432464,  0.        ,  0.        ,  0.84567541],
+       [ 0.15158875,  0.        ,  0.        ,  0.8484112 ]],)
+    row_column_policy = k_200_policy.reshape([num_rows, num_columns, 4])
+
+    weights = np.maximum(
+        0.0,
+        np.random.normal(size=row_column_policy.shape[:2])
+    )
+    fig, ax = Gridworld.Painter.draw(
+        row_column_policy,
+        weights / weights.sum()
+    )
+
+    plt.show()
