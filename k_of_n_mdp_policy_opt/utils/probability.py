64eac0fb28fbac2c43b1a2bced3553a05d1a9a39
Dustin Morrill <dmorrill10@gmail.com>
2018-06-19 15:53:39 -0600

    Improve style.

--- k_of_n_mdp_policy_opt/utils/probability.py
+++ k_of_n_mdp_policy_opt/utils/probability.py
@@ -10,7 +10,7 @@ def prob_action(state_distribution, strat):
     Returns:
     - Rank-1 Tensor (|A|). Prob of taking each action.
     '''
-    return tf.tensordot(strat, state_distribution, axes=((0,), (0,)))
+    return tf.tensordot(strat, state_distribution, axes=((0, ), (0, )))
 
 
 def prob_state_and_action(state_distribution, strat=None, num_actions=None):
@@ -53,21 +53,19 @@ def prob_state_given_action(state_distribution, strat=None, num_actions=None):
         state_distribution,
         strat=strat,
         num_actions=num_actions
-    )
+    )  # yapf:disable
     prob_action_ = tf.reduce_sum(prob_state_and_action_, axis=0)  # |A|
     prob_action_ = tf.where(
         tf.greater(prob_action_, 0.0),
         prob_action_,
         tf.constant(1.0, shape=prob_action_.shape)
-    )
+    )  # yapf:disable
     return tf.transpose(prob_state_and_action_ / prob_action_)  # |A| by |S|
 
 
-def prob_next_state_given_action(
-    transition_model,
-    state_distribution,
-    strat=None
-):
+def prob_next_state_given_action(transition_model,
+                                 state_distribution,
+                                 strat=None):
     return tf.transpose(
         tf.reduce_sum(
             (
@@ -80,7 +78,7 @@ def prob_next_state_given_action(
             ),
             axis=2
         )
-    )
+    )  # yapf:disable
 
 
 def prob_next_state(transition_model, state_distribution, strat=None):
@@ -92,4 +90,4 @@ def prob_next_state(transition_model, state_distribution, strat=None):
             num_actions=transition_model.shape[1].value
         ),
         axes=((0, 1), (0, 1))
-    )
+    )  # yapf:disable
