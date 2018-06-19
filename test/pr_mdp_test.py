0c99b2003f74838c276b391efecd7790cd362ba0
Dustin Morrill <dmorrill10@gmail.com>
2018-06-19 15:18:36 -0600

    Improve `pr_mdp.mean_models` and style.

--- test/pr_mdp_test.py
+++ test/pr_mdp_test.py
@@ -1,51 +1,66 @@
 import tensorflow as tf
 import numpy as np
-from k_of_n_mdp_policy_opt.pr_mdp import pr_mdp_rollout, pr_mdp_expected_value, \
-    pr_mdp_optimal_policy_and_value
+from k_of_n_mdp_policy_opt.pr_mdp import \
+    pr_mdp_rollout, \
+    pr_mdp_expected_value, \
+    pr_mdp_optimal_policy_and_value, \
+    mean_models
 from k_of_n_mdp_policy_opt.utils.sequence import num_pr_sequences
 from amii_tf_nn.projection import l1_projection_to_simplex
 
 
 class PrMdpTest(tf.test.TestCase):
-    def test_update(self):
-        with self.test_session() as sess:
-            horizon = 2
-            num_states = 3
-            num_actions = 2
+    def setUp(self):
+        np.random.seed(42)
+        tf.set_random_seed(42)
 
-            transition_model = tf.transpose(
-                l1_projection_to_simplex(
-                    tf.transpose(
-                        tf.random_normal(
-                            (num_states, num_actions, num_states)
-                        )
-                    )
-                )
-            )
-            root = l1_projection_to_simplex(tf.constant([1, 2, 3.0]))
-            x_update_1 = [
+    def test_mean_models(self):
+        a = np.random.normal(size=[3, 2]).astype('float32')
+        b = np.random.normal(size=[3, 2]).astype('float32')
+
+        with self.test_session():
+            self.assertAllClose((a + b) / 2.0, mean_models(a, b)[0])
+
+            patient = mean_models((a, b), (b, 2 * a), n=2)
+            self.assertAllClose((a + b) / 2.0, patient[0])
+            self.assertAllClose((2 * a + b) / 2.0, patient[1])
+
+    def test_update(self):
+        horizon = 2
+        num_states = 3
+        num_actions = 2
+
+        transition_model = tf.transpose(
+            l1_projection_to_simplex(
+                np.random.normal(
+                    size=(num_states, num_actions, num_states)
+                ).T
+            ))  # yapf:disable
+        root = l1_projection_to_simplex(tf.constant([1, 2, 3.0]))
+        x_update_1 = np.array(
+            [
                 [
                     [
-                        [0.15077846, 0., 0.01588821],
-                        [0.02819305, 0.13847362, 0.]
+                        [0.07233965396881104, 0.0, 0.09432701766490936],
+                        [0.1666666716337204, 0.0, 0.0]
                     ],
                     [
-                        [0.04897123, 0.28436211, 0.],
-                        [0., 0.33333334, 0.]
+                        [0.22432182729244232, 0.10901150107383728, 0.0],
+                        [0.3333333432674408, 0.0, 0.0]
                     ],
                     [
-                        [0., 0.33023661, 0.16976337],
-                        [0.5, 0., 0.]
+                        [0.5, 0.0, 0.0],
+                        [0.0, 0.0, 0.5]
                     ]
                 ]
             ]
+        )  # yapf:disable
 
-            self.assertAllClose(
-                tf.constant(x_update_1).eval(),
-                sess.run(
-                    pr_mdp_rollout(horizon, root, transition_model)
-                )[:1, :, :, :]
-            )
+        with self.test_session():
+            patient = pr_mdp_rollout(horizon, root,
+                                     transition_model)[:1, :, :, :]
+
+            self.assertAllClose(x_update_1, patient)
             # TODO Check that the rest of the sequences were updated properly.
 
     def test_expected_value(self):
@@ -56,9 +71,7 @@ class PrMdpTest(tf.test.TestCase):
 
             transition_model = tf.transpose(
                 l1_projection_to_simplex(
-                    tf.zeros((num_states, num_actions, num_states))
-                )
-            )
+                    tf.zeros((num_states, num_actions, num_states))))
             rewards = np.ones((num_states, num_actions, num_states))
             root = l1_projection_to_simplex(tf.ones([3]))
             uniform_random_strat = tf.transpose(
@@ -71,10 +84,7 @@ class PrMdpTest(tf.test.TestCase):
                                 num_states,
                                 num_actions
                             )
-                        )
-                    )
-                )
-            )
+                        ))))  # yapf:disable
             self.assertAlmostEqual(
                 2.0,
                 sess.run(
@@ -87,8 +97,7 @@ class PrMdpTest(tf.test.TestCase):
                         uniform_random_strat
                     )
                 ),
-                places=6
-            )
+                places=6)  # yapf:disable
 
     def test_expected_value_2(self):
         with self.test_session() as sess:
@@ -103,10 +112,7 @@ class PrMdpTest(tf.test.TestCase):
                             num_states,
                             num_actions,
                             num_states
-                        )
-                    )
-                )
-            )
+                        ))))  # yapf:disable
             rewards = (
                 np.random.normal(
                     loc=-1.0,
@@ -125,7 +131,7 @@ class PrMdpTest(tf.test.TestCase):
                         )
                     )
                 )
-            )
+            )  # yapf:disable
             root = l1_projection_to_simplex(tf.ones([3]))
             uniform_random_strat = tf.transpose(
                 l1_projection_to_simplex(
@@ -136,21 +142,16 @@ class PrMdpTest(tf.test.TestCase):
                                 horizon - 1,
                                 num_states,
                                 num_actions
-                            )
-                        )
-                    )
-                )
-            )
+                            )))))  # yapf:disable
             ev = pr_mdp_expected_value(
                 horizon,
                 num_states,
                 num_actions,
                 pr_mdp_rollout(horizon, root, transition_model),
                 rewards,
-                uniform_random_strat
-            )
-            self.assertAlmostEqual(-0.62523419, sess.run(ev))
-            self.assertAlmostEqual(-0.62523419, sess.run(ev))
+                uniform_random_strat)  # yapf:disable
+            self.assertAlmostEqual(-0.75842464, sess.run(ev))
+            self.assertAlmostEqual(-0.75842464, sess.run(ev))
 
     def test_best_response(self):
         with self.test_session() as sess:
@@ -165,10 +166,7 @@ class PrMdpTest(tf.test.TestCase):
                             num_states,
                             num_actions,
                             num_states
-                        )
-                    )
-                )
-            )
+                        ))))  # yapf:disable
             rewards = (
                 np.random.normal(
                     loc=-1.0,
@@ -187,16 +185,15 @@ class PrMdpTest(tf.test.TestCase):
                         )
                     )
                 )
-            )
+            )  # yapf:disable
             root = l1_projection_to_simplex(tf.ones([3]))
             br_strat, br_val = pr_mdp_optimal_policy_and_value(
                 horizon,
                 num_states,
                 num_actions,
                 pr_mdp_rollout(horizon, root, transition_model),
-                rewards
-            )
-            self.assertAlmostEqual(-0.35074115, sess.run(br_val))
+                rewards)  # yapf:disable
+            self.assertAlmostEqual(-0.24534382, sess.run(br_val))
 
 
 if __name__ == '__main__':
