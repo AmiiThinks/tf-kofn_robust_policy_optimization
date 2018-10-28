92868e5d00472fecfc69f55948063a498f45e4be
Dustin Morrill <dmorrill10@gmail.com>
2018-10-28 13:45:15 -0600

    Extract batch dimension standardization to its own function.

--- tf_kofn_robust_policy_optimization/utils/tensor.py
+++ tf_kofn_robust_policy_optimization/utils/tensor.py
@@ -2,6 +2,33 @@ import numpy as np
 import tensorflow as tf
 
 
+def standardize_batch_dim(*tensors_with_ndims):
+    tensors_with_ndims = [(tf.convert_to_tensor(t), ndims)
+                          for t, ndims in tensors_with_ndims]
+    tensors = [t for t, _ in tensors_with_ndims]
+    has_batch_dim_list = [
+        len(t.shape) > ndims for t, ndims in tensors_with_ndims
+    ]
+    has_batch_dim = any(has_batch_dim_list)
+
+    if has_batch_dim:
+        batch_sizes = [
+            t.shape[0].value if has_batch_dim_list[i] else 1
+            for i, (t, _) in enumerate(tensors_with_ndims)
+        ]
+        max_batch_size = max(batch_sizes)
+
+        for i, (t, ndims) in enumerate(tensors_with_ndims):
+            if not has_batch_dim_list[i]:
+                t = tf.tile(
+                    tf.expand_dims(t, 0), [max_batch_size] + [1] * ndims)
+            elif batch_sizes[i] < max_batch_size:
+                t = tf.tile(t,
+                            [max_batch_size // batch_sizes[i]] + [1] * ndims)
+            tensors[i] = t
+    return tensors, has_batch_dim
+
+
 def block_ones(num_blocks, block_width):
     '''
     Returns a block_width-wide block-diagonal matrix of ones with dimension
